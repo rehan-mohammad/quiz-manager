799ba3ab9a5c6fb2c1d9934440d62865d30ab486
rehanmohammad <rehan@develodesign.co.uk>
2020-04-07 12:04:23 +0100

    added users page link to admin

--- resources/views/admin/menu.blade.php
+++ resources/views/admin/menu.blade.php
@@ -24,3 +24,12 @@
     </a>
 
 </li>
+
+<li class="nav-item">
+
+    <a class="nav-link {{ Request::is('users*') ? 'active' : '' }}"
+       href="{!! route('users.index') !!}" role="button" aria-haspopup="true" aria-expanded="false">
+        Users
+    </a>
+
+</li>
