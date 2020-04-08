a7cff274cb3a851ea5067519bdb49b714fcc2bd1
rehanmohammad <rehan@develodesign.co.uk>
2020-04-08 10:56:57 +0100

    updated permission labels

--- resources/views/admin/users/fields.blade.php
+++ resources/views/admin/users/fields.blade.php
@@ -32,7 +32,7 @@
 
     {!! Form::label('is_admin', 'Permissions:') !!}
 
-    {!! Form::select('is_admin', ["1"=>"Admin", "0"=>"Guest", "2" => "Limited"] , null, ['class' => 'form-control']) !!}
+    {!! Form::select('is_admin', ["1"=>"Edit", "0"=>"View", "2" => "Restricted"] , null, ['class' => 'form-control']) !!}
 
     {!! $errors->first('is_admin', '<div class="form-control-feedback">:message</div>') !!}
 
